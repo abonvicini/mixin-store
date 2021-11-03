6e4ad8cd6accc61e78b48ee94dfe6b8066f32d35
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-11-03 01:52:40 -0300

    Improved style code

---
+++ src/components/Container/CartViewContainer/styles/index.js
@@ -0,0 +1,23 @@
+export const containerCart = {
+  display: 'flex',
+  flexDirection: 'column',
+  marginTop: '4em',
+  justifyContent: 'center',
+};
+export const containerCardCart = {
+  display: 'grid',
+  gap: '1rem',
+  gridTemplateColumns: 'repeat(auto-fit, 300px)',
+  placeContent: 'center',
+  marginBottom: '2em',
+};
+
+export const textCardCart = {
+  margin: '1rem',
+  padding: '1rem',
+  marginRight: '4rem',
+  fontSize: '2rem',
+  fontWeight: 600,
+  color: '#333',
+  alignSelf: 'flex-end',
+};
