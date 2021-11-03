231ccfdc29c2b2d7cbb1ad94ecf46191953ca988
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-11-03 01:59:08 -0300

    New navbar component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 import './App.css';
 import ItemDetailContainer from './components/Container/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/Container/ItemListContainer/ItemListContainer';
-import NavBar from './components/NavBar/NavBar';
+// import NavBar from './components/NavBar/NavBar';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import CartProvider from './context/CartContext';
+import AuthProvider from 'context/AuthContext';
 import CartView from './components/Container/CartViewContainer/CartViewContainer';
+import MuiNavBar from './components/NavBar/MuiNavBar';
 
 function App() {
   return (
     <div className="App">
-      <CartProvider>
-        <BrowserRouter>
-          <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <ItemListContainer />
-            </Route>
+      <AuthProvider>
+        <CartProvider>
+          <BrowserRouter>
+            {/* <NavBar /> */}
+            <MuiNavBar />
+            <Switch>
+              <Route exact path="/">
+                <ItemListContainer />
+              </Route>
 
-            <Route exact path="/category/:categoryId">
-              <ItemListContainer />
-            </Route>
+              <Route exact path="/category/:categoryId">
+                <ItemListContainer />
+              </Route>
 
-            <Route exact path="/detail/:itemId">
-              <ItemDetailContainer />
-            </Route>
+              <Route exact path="/detail/:itemId">
+                <ItemDetailContainer />
+              </Route>
 
-            <Route exact path="/contacto">
-              <h1>Contacto</h1>
-            </Route>
+              <Route exact path="/contacto">
+                <h1>Contacto</h1>
+              </Route>
 
-            <Route exact path="/cart">
-              <CartView />
-            </Route>
+              <Route exact path="/cart">
+                <CartView />
+              </Route>
 
-            <Route path="*">
-              <Redirect to="/" />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </CartProvider>
+              <Route path="*">
+                <Redirect to="/" />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </CartProvider>
+      </AuthProvider>
     </div>
   );
 }
